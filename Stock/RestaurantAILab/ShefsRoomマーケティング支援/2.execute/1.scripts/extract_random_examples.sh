a9b449eefa0f0e981c08b882845be4fe9beb4333
Riku Tanaka <restaurant.ai.lab@gmail.com>
2025-07-21 07:54:07 +0900

    5/20

---
+++ Stock/RestaurantAILab/ShefsRoomマーケティング支援/2.execute/1.scripts/extract_random_examples.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+INPUT="1.scripts/examples.yaml"
+
+awk '
+/^- title:/ {
+  if (block) {
+    blocks[++n] = block
+    block = ""
+  }
+}
+{ block = block $0 ORS }
+END {
+  if (block) blocks[++n] = block
+  srand()
+  for (i=1; i<=n; i++) order[i]=i
+  for (i=n; i>1; i--) {
+    j = int(rand()*i)+1
+    tmp=order[i]; order[i]=order[j]; order[j]=tmp
+  }
+  for (i=1; i<=5 && i<=n; i++) print blocks[order[i]]
+}
+' "$INPUT" 
